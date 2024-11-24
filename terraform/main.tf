d99926a5adb5d934b033943b62edd65a65d78ab4
Mohammad Rayhan <mroyhan95@gmail.com>
2024-11-24 16:09:58 +0700

    add setup ACR&AKS terraform and deploy job in workflow

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,6 +21,7 @@ resource "azurerm_resource_group" "ntx_devops_group" {
   location = var.azure_resource_group_location
 }
 
+## Setup Azure Container Registry
 resource "azurerm_container_registry" "ntx_devops_registries" {
   name                = var.acr_name
   resource_group_name = azurerm_resource_group.ntx_devops_group.name
@@ -34,15 +35,43 @@ output "acr_name" {
   value = azurerm_container_registry.ntx_devops_registries.name
 }
 
-#output "acr_login_server" {
-#  value = azurerm_container_registry.ntx_devops_registries.login_server
-#}
-#
-#output "acr_admin_username" {
-#  value = azurerm_container_registry.ntx_devops_registries.admin_username
-#}
-#
-#output "acr_admin_password" {
-#  value = azurerm_container_registry.ntx_devops_registries.admin_password
-#  sensitive = true
-#}
+# Setup Azure Kubernetes Registry
+resource "azurerm_kubernetes_cluster" "ntx_aks" {
+  name                = "ntx-aks"
+  location            = azurerm_resource_group.ntx_devops_group.location
+  resource_group_name = azurerm_resource_group.ntx_devops_group.name
+  dns_prefix          = "ntxaks"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "test"
+  }
+
+  depends_on = [azurerm_container_registry.ntx_devops_registries]
+
+}
+
+resource "azurerm_role_assignment" "ntx_aks_role" {
+  principal_id                     = azurerm_kubernetes_cluster.ntx_aks.identity[0].principal_id
+  role_definition_name             = "AcrPull"
+  scope                            = azurerm_container_registry.ntx_devops_registries.id
+}
+
+# Output for AKS name
+output "aks_name" {
+  value = azurerm_kubernetes_cluster.ntx_aks.name
+}
+
+# Output for AKS resource group
+output "aks_resource_group" {
+  value = azurerm_resource_group.ntx_devops_group.name
+}
