d912d741b6046c2c4571789dce093ae8f5dcaf80
Mohammad Rayhan <mroyhan95@gmail.com>
2024-11-24 13:44:37 +0700

    fix variables.tf undeclared value

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,23 +1,3 @@
-variable "azure_subscription_id" {
-  description = "subscrption id azure"
-  type = string
-}
-
-variable "azure_tenant_id" {
-  description = "tenant id azure"
-  type = string
-}
-
-variable "terraform_azure_client_id" {
-  description = "azure terraform user subscription id"
-  type = string
-}
-
-variable "terraform_azure_client_secret" {
-  description = "azure terraform user subscription secret"
-  type = string
-}
-
 variable "azure_resource_group_name" {
   description = "azure resource group name"
   type = string
