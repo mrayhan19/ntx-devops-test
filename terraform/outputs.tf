7193d8e7ebb5b32b36890516e5961c889fdc8647
Mohammad Rayhan <mroyhan95@gmail.com>
2025-05-23 21:14:20 +0700

    remove output lb ip

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,3 @@
 output "artifact_repo_url" {
   value = google_artifact_registry_repository.docker_repo.repository_id
 }
-
-output "load_balancer_ip" {
-  value = kubernetes_service.app_svc.status[0].load_balancer[0].ip
-}
